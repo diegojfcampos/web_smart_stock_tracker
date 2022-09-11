a9b28a5c0a48f6f8961e09762907a6ecea3ddea8
diegojfcampos <diegojfcampos@gmail.com>
2022-09-11 01:06:55 +0100

    Creating main table

--- js/main.js
+++ js/main.js
@@ -6,70 +6,57 @@ axios.defaults.baseURL = "http://localhost:8000";
 const urlUSD = "/getcryptos/usd";
 const urlFilteredBTC = "/getcryptos/btcfilter";
 
-function getCryptos(){
-    axios.get(urlApiCoinGeck)
+
+let getfilteredBtc = () => {
+        axios.get(urlFilteredBTC)
         .then(response => {
-            const data = response.data;
-            console.log(typeof(data));
+            const data = response.data;           
+            createMainTable(data)      
+        }).catch(error => console.log(error))
+        
+} 
 
-            console
-            
-            console.log(data);
-            console.log(data[0].id);
-
-           JSON.stringify(data);
-            
-        })
-        .catch(error => console.log(error))
+function createCel(tag, text){
+    let cel = document.createElement(tag);
+    cel.textContent = text;
+    return cel; 
 }
-
-function getFilteredBtc(){
-    axios.get(urlFilteredBTC)
-    .then(response =>{
-        const filteredBTC = response.data;
-        JSON.stringify(filteredBTC)
-        return filteredBTC;
-
-    })
-    .catch(error => console.log(error))
-}
-getFilteredBtc();
 
 //Creating Table
+function createMainTable(data){
 
-let currencyTable = document.getElementById("currencyTable");
+    let currencyTable = document.getElementById("currencyTable");
+    let thead = document.createElement("thead");
+    let tbody = document.createElement("tbody");
+ 
+    let headList = [" Rank ", " Coin ", " Price ", " 24 Change ", " Market Cap ", " Volume"];
+    let rowHead = document.createElement("tr");
 
-function addElement(element){
-    return document.createElement(element)
-}
-
-let thead = addElement("thead");
-let tbody = addElement("tbody");
-currencyTable.appendChild(thead);
-currencyTable.appendChild(tbody);
-
-let headList = [" Rank ", " Coin ", " Price ", " 24 Change ", " Market Cap ", " Volume"];
-let rowHead = addElement("tr");
-
-for(var head = 0; head <= headList.length; head++){
-    let th = addElement("th");
-    th.textContent = headList[head];
-    rowHead.appendChild(th);
-}
-thead.appendChild(rowHead);
-
-let currencyData = getFilteredBtc();
-console.log(JSON.stringify(currencyData));
-console.log(typeof(currencyData));
-
-for (let body = 0; body <= currencyData.length; row++){
-    bodyRow = addElement("tr")
-    
-    for(let cel = 0; cel <= currencyData[body]; cel++){
-        bodyCel = addElement("td")
-        contentCel = currencyData[row][cel];
-        bodyRow.appendChild(bodyCel, contentCel);        
+    for(var head = 0; head <= headList.length; head++){
+        let th = document.createElement("th");
+        th.textContent = headList[head];
+        rowHead.appendChild(th);
     }
-    tbody.appendChild(bodyRow);
+    thead.appendChild(rowHead);
+    currencyTable.appendChild(thead);
+    
+    /**console.log(data)
+    console.log(typeof(data))    
+    console.log(data.toString([0]))
+    */
+
+    for (let body = 0; body <= data.length; body++){
+        let bodyRow = document.createElement("tr");
+        console.log("In the body")
+        for(let cel = 0; cel < data.length; cel++){
+            let bodyCel = createCel("td", data[body][cel]);        
+           
+            bodyRow.appendChild(bodyCel);        
+        }
+        tbody.appendChild(bodyRow);
+    }    
+    currencyTable.appendChild(tbody);
 }
 
+
+getfilteredBtc();
